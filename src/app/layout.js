21a466759dc46eecffaba3b613fd36e1f73d2b8e
Ayush-3012 <diveshsrivastava31@gmail.com>
2025-05-29 14:30:05 +0530

    added icon and title

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,10 @@ import Navbar from "./components/Navbar";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <title>Box-Office NextJs</title>
+        <link rel="icon" href="./icon.png" />
+      </head>
       <body suppressHydrationWarning>
         <div className="px-4 m-4 rounded-xl overflow-y-scroll bg-slate-700 max-md:m-2 max-sm:m-0.5">
           <Navbar />
